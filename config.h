d5170174365152d479bbca07410ba18e6942acb4
zaid <zaidde2005@gmail.com>
2022-11-12 16:55:48 +0200

    add screen shot

--- config.h
+++ config.h
@@ -61,14 +61,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *shutdown[] = { "shutdown", "now"};
+static const char *shot[] = { "magick", "import /home/zaid/shot.jpg"};
 static const char *lock[] = { "slock", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ Mod4Mask,                  XK_s,      spawn,          {.v = shutdown} },
-	{ Mod4Mask,                  XK_l,      spawn,          {.v = lock} },
+	{ Mod4Mask,                     XK_s,      spawn,          {.v = shutdown} },
+	{ Mod4Mask,                     XK_l,      spawn,          {.v = lock} },
+	{ Mod4Mask,                     XK_p,      spawn,          {.v = shot} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
