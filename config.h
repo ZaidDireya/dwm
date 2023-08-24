9a73a54e2d83376dbdcbab752cc0f3ab917cb5df
zaid <zaidde2005@gmail.com>
2023-08-24 19:29:56 +0300

    add screen shot shortcut

--- config.h
+++ config.h
@@ -64,14 +64,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *shutdown[] = { "shutdown", "now"};
-static const char *shot[] = { "import", "/home/zaid/shot.jpg"};
+static const char *shot[] = { "flameshot", "gui", NULL};
 static const char *lock[] = { "slock", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ Mod4Mask,                     XK_s,      spawn,          {.v = shutdown} },
+	{ Mod4Mask,                     XK_s,      spawn,          {.v = shutdown } },
 	{ Mod4Mask,                     XK_l,      spawn,          {.v = lock} },
 	{ Mod4Mask,                     XK_p,      spawn,          {.v = shot} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
